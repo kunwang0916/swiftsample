702350bf8a171d25856f8b07ee641258ea8292ee
kun wang <kunwangus@gmail.com>
2019-06-15 20:55:17 -0700

    adding ios_tips

---
+++ ios_tips/ios_tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_tips
+//
+//  Created by roosky on 6/15/19.
+//  Copyright © 2019 K W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios_tips/ios_tipsTests/ios_tipsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_tipsTests.swift
+//  ios_tipsTests
+//
+//  Created by roosky on 6/15/19.
+//  Copyright © 2019 K W. All rights reserved.
+//
+
+import XCTest
+@testable import ios_tips
+
+class ios_tipsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
